92a6113acc90eab4a177cca04317b8b7cd85ca78
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-08 14:19:33 -0700

    wrote and compiled c programs

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	int b;
+	long c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d\nSize of an int: %d\nSize of a long int: %d\nSize of a long long int: %d\nSize of a float: %d\n",sizeof(a),sizeof(b),sizeof(c),sizeof(d),sizeof(e));
+	return (0);
+}
