aac6b10daf4e2eda14866436fb81cad4593c62ff
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-08 14:59:10 -0700

    made few corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fwrite(hello, sizeof(hello) - 1, 1, stdout);
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(void) /*prints size of types*/
 {
 	char a;
 	int b;
 	long c;
 	long long int d;
 	float e;
-	printf("Size of a char: %d\nSize of an int: %d\nSize of a long int: %d\nSize of a long long int: %d\nSize of a float: %d\n",sizeof(a),sizeof(b),sizeof(c),sizeof(d),sizeof(e));
+
+	printf("Size of a char: %d\n", sizeof(a));
+	printf("Size of an int: %d\n", sizeof(b));
+	printf("Size of a long int: %d\n", sizeof(c));
+	printf("Size of a long long int: %d\n", sizeof(c));
+	printf("Size of a float: %d\n", sizeof(e));
 	return (0);
 }
