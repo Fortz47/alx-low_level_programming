bf5d5677cd420a3f5053704cb27c9b0cc6d16e86
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-08 23:56:07 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase stored in h
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	const char h[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(h, sizeof(h) - 1, 1, stdout);
-	return (0);
+	return (1);
 }
