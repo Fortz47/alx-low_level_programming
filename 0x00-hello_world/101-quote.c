d1d4a50e6b73d1443630a33435b0227183351f84
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-08 23:55:08 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char h[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(hello, sizeof(hello) - 1, 1, stdout);
+	fwrite(h, sizeof(h) - 1, 1, stdout);
 	return (0);
 }
