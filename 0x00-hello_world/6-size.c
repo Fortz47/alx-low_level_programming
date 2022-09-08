a00cc2c042f51323d996c40d56ae1c75fe5e61d2
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-08 23:39:12 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %d\n", sizeof(a));
 	printf("Size of an int: %d\n", sizeof(b));
 	printf("Size of a long int: %d\n", sizeof(c));
-	printf("Size of a long long int: %d\n", sizeof(D));
+	printf("Size of a long long int: %d\n", sizeof(d));
 	printf("Size of a float: %d\n", sizeof(e));
 	return (0);
 }
