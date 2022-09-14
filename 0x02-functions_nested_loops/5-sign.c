f5cba503b5ad0083e0ff45ab32df880447800f6b
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-14 17:57:03 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,8 +11,8 @@ int print_sign(int n)
 {
 	if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else if (n > 0)
 	{
@@ -21,8 +21,8 @@ int print_sign(int n)
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	return (0);
 }
