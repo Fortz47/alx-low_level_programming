5ea3f2b75727a16c729ab148402a11dad5a6f90a
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-15 07:45:00 -0700

    wrote the code for 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number n
+ * @n: function parameter
+ * Return: c
+ */
+int print_last_digit(int n)
+{
+	int c = n % 10;
+
+	return (c);
+}
