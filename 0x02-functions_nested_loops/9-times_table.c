8ba4cc2ef8e03d21156684956bb8d654cd7a03cc
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-15 07:42:59 +0100

    Update 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ * @void: takes no argument
+ * Return: void.
+ */
+ 
+void times_table(void)
+{
+	int i, n, mul;
+	i = 0;
+
+	while (i < 10)
+	{
+		for (n = 0 ; n < 10; n++)
+		{
+			mul = i * n;
+			if (n == 0)
+				_putchar(mul + '0')
+			if (mul < 10 && n != 0)
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
+				_putchar(mul + '0');
+			}
+			else if (mul >=10 && mul <81)
+			{
+				_putchar(',');
+				_putchar(32);
+				putchar((mul / 10) + '0');
+				putchar((mul % 10) + '0');
+			}
+		}
+		i++;
+		_putchar('\n');
+	}
+}
