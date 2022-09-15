6e90881616a02694c7245c9c4af35b7c876a593e
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-15 03:58:13 -0700

    wrote code for 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @n1 and @n2 (function parameters)
+ * Return: returns the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	int n = n1 + n2;
+
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from
+ * n to 98 followed by a newline
+ * @n: function parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i, j, k;
+
+	if (n == 98)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+	}
+	else if (n < 0)
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+		printf("%d\n", 98);
+	}
+	else if (n >= 0)
+	{
+		for (j = n; j < 98; j++)
+			printf("%d, ", j);
+		printf("%d\n", 98);
+	}
+	else if (n > 98)
+	{
+		k = n;
+
+		while (k > n && k != 98)
+		{
+			printf("%d, ", k);
+			k--;
+		}
+		printf("%d\n", 98);
+
+	}
+}
