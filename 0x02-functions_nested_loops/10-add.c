91c1fb7cfaed7d3b1eb4b9d5065a0cd2c0857ae7
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-15 16:03:10 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - check the code
- * @n1 and @n2 (function parameters)
+ * @n1: (function parameter)
+ * @n2: function parameter
  * Return: returns the sum of n1 and n2
  */
 int add(int n1, int n2)
