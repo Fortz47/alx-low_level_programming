b647a336a6511dad54b2c7b5c0ee245e993c8a34
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-16 01:42:23 -0700

    wrote code for 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k, z;
+
+	i = 1;
+	j = 2;
+	k = 1;
+	z = 0;
+	while (i > 0 && z <= 4000000)
+	{
+		j = j + k;
+		k = j - k
+		if ((j % 2) == 0)
+			z = z + j;
+		i++;
+	}
+	return (0);
+}
