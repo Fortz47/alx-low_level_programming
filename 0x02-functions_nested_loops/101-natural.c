1d7dc0d942fda5b67ba85320493e129750e0577e
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-16 07:21:44 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
  * main - entry point
  * description: computes and prints the sum of all the multiples
  * of 3 or 5 below 1024 (excluded) followed by a new line
- * return: 0 (success))
+ * Return: 0 (success)
  */
 int main(void)
 {
