fbd293cf0ce4aa2cb7ab978c726b5d38540969f5
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-16 00:18:46 -0700

    wrote code for 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 3;
+	j = 2;
+	k = 1;
+	printf("1, 2");
+	while (i <= 50)
+	{
+		j = j + k;
+		k = j - k;
+		printf(", %d", j);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
