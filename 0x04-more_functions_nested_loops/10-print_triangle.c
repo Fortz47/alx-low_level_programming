c44de65628e252311b3486b463940e44a2a56032
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-17 09:09:40 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 		while (i <= size)
 		{
 			for (j = i; j < size; j++)
-				_putchar('_');
+				_putchar(' ');
 			for (j = 1; j <= i; j++)
 				_putchar('#');
 			i++;
