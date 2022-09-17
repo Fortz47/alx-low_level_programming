1d93587e88f0c0fe1f219ec58969582c5cdcc85a
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-17 11:43:10 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = -2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	return (0);
+}
