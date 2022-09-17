a1afada78e125b03aafc5a2c6831c58cf24071af
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-17 07:44:28 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-/*#ifndef MAIN_H
-#define MAIN_H*/
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c); 
+/*int _putchar(char c); */
 int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
@@ -12,4 +12,4 @@ int mul(int a, int b);
 void print_triangle(int size);
 void print_number(int n);
 
-/*#endif*/
+#endif
