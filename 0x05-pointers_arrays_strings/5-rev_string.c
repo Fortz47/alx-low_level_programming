8e30bc1e2310fee54da54a709f14f7bbb6907262
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-19 16:28:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a string
+ * @s: function parameter
+ */
+
+void print_rev(char *s)
+{
+	char j, tmp;
+	int i, k, len;
+
+	j = '\0';
+	i = 0;
+	len = 0;
+
+	while (s[i] != j)
+		i++;
+	len = i - 1;
+	for (k = 0; k < i / 2; k++)
+	{
+		tmp = s[k];
+		s[k] = s[len];
+		s[len--] = tmp;
+	}
+}
