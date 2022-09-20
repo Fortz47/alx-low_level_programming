ee50c6f1d31f1443667c75b357a37d041d947da3
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-09-20 08:10:43 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,15 @@ void puts2(char *str)
 	int i;
 
 	i = 1;
-	_putchar(str[0]);
-	while (str[i] != '\0')
+	if (*str != ' ')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		_putchar(str[0]);
+		while (str[i] != '\0')
+		{
+			if (i % 2 == 0)
+				_putchar(str[i]);
+			i++;
+		}
 	}
 	_putchar('\n');
 }
