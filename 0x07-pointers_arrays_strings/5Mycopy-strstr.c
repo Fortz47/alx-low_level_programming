a1eb052c73b6e155d9532234f85eccbd488ac1ef
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-09-28 02:02:42 -0700

    update

---
+++ 0x07-pointers_arrays_strings/5Mycopy-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, ptr;
+
+	i = 0;
+	j = 0;
+	ptr = 0;
+
+	for (; haystack[j] != '\0';)
+	{
+		if (needle[i] == haystack[j])
+		{
+			i++;
+			j++;
+
+			if (needle[i] == '\0')
+			{
+				j -= 1;
+				return (haystack + (j - ptr));
+			}
+			else
+			{
+				ptr++;
+				continue;
+			}
+		}
+		ptr = 0;
+		i = 0;
+		j++;
+	}
+	return (NULL);
+
+}
