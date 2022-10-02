a055baa9de780e6fbd8bcc7aed8e0114e60e0185
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-10-02 11:13:12 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: pointer to a char array
+ * description: adds the arguments of the main function
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+	if (argc == 1)
+		printf("0\n");
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+			else
+			{
+				sum = sum + argv[i];
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
