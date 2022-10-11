c227d7555b263c695ca8fc3b7b86867a1cce506c
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-10-11 09:49:29 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct
+ * @d: pointer to struct to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	int i;
+
+	char dog[4] = {"name", "age", "owner"};
+	char _dog[4] = {"Name", "Age", "Owner"};
+
+	if (d == NULL)
+		return (0);
+
+	for (i = 0; i < 3; i++)
+	{
+		if (d->dog[i] == NULL)
+			printf("%s: nil\n", _dog[i]);
+		else
+			printf("%s: %s\n", _dog[i], d->dog[i]);
+	}
+}
