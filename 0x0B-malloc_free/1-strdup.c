6b21d3a25a460a4a642aaa5c31cbf273ee44d338
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-10-11 02:05:12 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the lenght of a string
+ * @string: pointer to a string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	return (i);
+}
+
+/**
+ * _strdup - returns a pointer to the duplicate of a string
+ * @str: source string
+ *
+ * Return: pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	int lenght = _strlen(str);
+
+	p = malloc(sizeof(*p) * lenght);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < lenght; i++)
+		p[i] = str[i];
+
+	p[i] = '\0';
+
+	return (p);
+}
