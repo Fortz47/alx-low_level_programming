b14afdd7237ac2bde512be1de2c23c1807b03fc3
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-10-11 16:32:19 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,23 +3,22 @@
 
 /**
  * struct dog - basic info on a dog
- * dog_t - typedef for struct dog
  * @name: first member
  * @age: second member
  * @owner: third member
  *
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-}dog_t;
+};
 
 /**
  * dog_t - typedef for struct dog
  */
-/* typedef struct dog dog_t; */
+typedef struct dog dog_t;
 
 #endif
