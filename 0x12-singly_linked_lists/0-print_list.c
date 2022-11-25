d2011a980365426d1ee83161db237d52846be1df
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-11-25 04:33:40 -0800

    created all working files and wrote task 0 code

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the number elements of a list_t
+ * @h: pointer to head node
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr;
+
+	ptr = h;
+	while (ptr)
+	{
+		count++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else 
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+	return (count);
+}
