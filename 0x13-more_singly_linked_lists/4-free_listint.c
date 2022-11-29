d73eee912c3b4ff4759ff62fb75fa5ad280dd45d
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-11-29 07:07:07 -0800

    solved task 2 to 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to first node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	ptr = head;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		free(head);
+		head = ptr;
+	}
+}
