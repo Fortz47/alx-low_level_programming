8606d133975c5f739ebef3bea285aa406f51f203
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-11-29 16:15:48 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
+	if (*head == NULL)
+		return;
 	ptr = *head;
 
 	while (ptr)
@@ -17,5 +19,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = ptr;
 	}
-	*head = NULL;
 }
