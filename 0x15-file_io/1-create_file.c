60a66c1ab06a8781cef4117d2411945060825ad1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-12-06 07:32:55 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,22 +10,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd_open, fd_write;
+	int i = 0, fd_open, fd_write;
 
 	if (filename == NULL)
 		return (-1);
-
-	i = 0;
-	while (text_content[i] != '\0')
-		i++;
+	if (text != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 
 	fd_open = open("filename", O_CREAT | O_RDWR | O_TRUNC, 600);
 	fd_write = write(fd_open, text_content, i);
 
 	if (fd_open == -1 || fd_write == -1)
-	{
 		return (-1);
-	}
+
 	close(fd_open);
 
 	return (1);
