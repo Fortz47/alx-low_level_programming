53cf740838704995a7e5ac8254498a4e1e2d5e9e
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-12-06 22:11:27 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,6 +29,12 @@ int main(int ac, char **av)
 	o_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 
 	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+		
 	r_1 = read(o_1, buffer, 1024);
 	if (r_1 == -1)
 	{
