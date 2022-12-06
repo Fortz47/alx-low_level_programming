6479ef7d9c969cd3631686cd46cd26b6f9caec56
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-12-06 07:27:06 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: content to write in file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd_open, fd_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	i = 0;
+	while (text_content[i] != '\0')
+		i++;
+
+	fd_open = open("filename", O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd_write = write(fd_open, text_content, i);
+
+	if (fd_open == -1 || fd_write == -1)
+	{
+		return (-1);
+	}
+	close(fd_open);
+
+	return (1);
+}
