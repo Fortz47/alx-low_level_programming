200e1669699fd75e23a1d4e91aeb62d8f735f380
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2022-12-12 08:42:23 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * b - prints a string before the main function
+ * smth - prints a string before the main function
  * is executed
  */
 
-void b(void) __attribute__ ((constructor));
-void b(void)
+void smth(void) __attribute__ ((constructor));
+void smth(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
