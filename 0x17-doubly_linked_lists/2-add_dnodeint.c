ab5de9568b41e32e92ce66966b1c6a4d94b32d19
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-01-13 13:35:36 -0800

    2-add_dnodeint.c ...

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
  * @head: pointer to first node
+ * @n: value of new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
@@ -13,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
-		return NULL;
+		return (NULL);
 	new->n = n;
 	new->next = ptr;
 	new->prev = NULL;
