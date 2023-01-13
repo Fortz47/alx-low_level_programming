7cc7ef1a6eea3101c2df5e1516a63f607478a834
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-13 21:41:23 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,11 +12,14 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *ptr = h;
 	int count = 0;
 
-	while (ptr != NULL)
+	if (head)
 	{
-		ptr = ptr->next;
-		count++;
-		printf("%d\n", ptr->n);
+		while (ptr != NULL)
+		{
+			ptr = ptr->next;
+			count++;
+			printf("%d\n", ptr->n);
+		}
 	}
 	return (count);
 }
