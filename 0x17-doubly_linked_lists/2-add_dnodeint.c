2d6a0dab2bab655dbd73d52190c7c35749dcf69c
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-01-13 22:45:01 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	*new->next = *head;
+	new->next = *head;
 	new->prev = NULL;
 	*head->prev = new;
 	*head = new;
