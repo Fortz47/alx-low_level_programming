330c8d763731f848da72ecc08c68a8907eb0d26c
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-06-07 23:07:57 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,12 +2,35 @@
 
 
 /**
+ * _sqrt_recursive_helper - returns the natural square root of a number.
+ * @n: parameter n
+ * @start: start range
+ * @end: end range
  *
+ * Return: square root of a number.
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_recursive_helper(int n, int start, int end)
 {
-	if (((x + (n / 2)) / 2) == x)
-		return (x);
-	_sqrt_recursion(n - 1);
+    if (start > end) {
+        return end;
+    }
+
+    int mid = start + (end - start) / 2;
+    int square = mid * mid;
+
+    if (square == n) {
+        return mid;
+    } else if (square > n) {
+        return _sqrt_recursive_helper(n, start, mid - 1);
+    } else {
+        return _sqrt_recursive_helper(n, mid + 1, end);
+    }
+}
+
+int _sqrt_recursive(int n)
+{
+    if (n < 0)
+        return -1;
+    return _sqrt_recursive_helper(n, 0, n);
 }
