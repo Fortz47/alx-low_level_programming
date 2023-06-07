b0e1f8532dbfe22940f3741b4ada590ba596e0e6
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-07 15:22:14 -0700

    updated 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,27 +12,34 @@
 
 int _sqrt_recursive_helper(int n, int start, int end)
 {
-    int mid, square;
+	int mid, square;
 
-    if (start > end) {
-        return end;
-    }
+	if (start > end)
+		return end;
 
-    mid = start + (end - start) / 2;
-    square = mid * mid;
+	mid = start + (end - start) / 2;
+	square = mid * mid;
 
-    if (square == n) {
-        return mid;
-    } else if (square > n) {
-        return _sqrt_recursive_helper(n, start, mid - 1);
-    } else {
-        return _sqrt_recursive_helper(n, mid + 1, end);
-    }
+	if (square == n)
+		return mid;
+	else if (square > n)
+		return _sqrt_recursive_helper(n, start, mid - 1);
+	else
+		return _sqrt_recursive_helper(n, mid + 1, end);
+
 }
+
+/**
+ * _sqrt_recursion - cal the square root of a number
+ * @n: number
+ *
+ * Return: square root of n
+ *
+ */
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    return _sqrt_recursive_helper(n, 0, n);
+	if (n < 0)
+		return -1;
+	return _sqrt_recursive_helper(n, 0, n);
 }
