1e1f471b02b9d3e1b7046d4e5acad02ad2e5d365
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-07 08:56:26 -0700

    solved task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void _print_rev_recursion - prints a string in reverse.
+ *
+ * @s: string to print
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_print_rev_recursion(s += 1);
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return;
+}
