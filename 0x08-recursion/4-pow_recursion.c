4afad3008ec6946149fea06a4463ad22287f62e5
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-07 14:48:10 -0700

    solved task 2 to 6

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - cal the val of x ^ y
+ * @x: x parameter
+ * @y: y parameter
+ *
+ * Return: x ^ y
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y -= 1));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (((x + (n / 2)) / 2) == x)
+		return (x);
+	_sqrt_recursion(n - 1);
+}
