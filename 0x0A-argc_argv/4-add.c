bb60b051197bbd4145ef69c2916c0baa7842e67f
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-09 13:06:27 -0700

    updated 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,12 +19,13 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
 		num = strtol(argv[i], &endptr, 10);
-		if (endptr != '\0')
+		if (*endptr != '\0')
 		{
 			printf("Error\n");
 			return (1);
