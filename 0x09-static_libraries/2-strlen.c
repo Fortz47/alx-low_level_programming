086b29e1fd364626d5265d634471840fb83f562d
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-09 11:27:57 -0700

    completed all task

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: parameter
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i, len;
+	char nullTerminator;
+
+	len = 0;
+	i = 0;
+	nullTerminator = '\0';
+
+	while (s[i] != nullTerminator)
+	{
+		len = len + 1;
+		i++;
+	}
+	return (len);
+}
