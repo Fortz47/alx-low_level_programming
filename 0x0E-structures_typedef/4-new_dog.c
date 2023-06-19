be2d642ac5a619098ecdda010241547ba8085fff
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-06-19 23:45:02 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 
 /**
+ * _strlen - cal lenght of a string
+ * @str: string
+ *
+ * Return: lenght of string
+ *
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
  * new_dog - creates a new dog.
  * @name: name
  * @age: age
@@ -17,9 +34,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!ptr)
 		return (NULL);
 
-	(*ptr).name = name;
+	ptr->name = malloc(_strlen(name) + 1);
+	ptr->owner = malloc(_strlen(owner) + 1);
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr->owner);
+		free(ptr);
+		return (NULL);
+	}
+	_strcpy(ptr->name, name);
+	_strcpy(ptr->owner, owner);
 	ptr->age = age;
-	(*ptr).owner = owner;
 
 	return (ptr);
 }
