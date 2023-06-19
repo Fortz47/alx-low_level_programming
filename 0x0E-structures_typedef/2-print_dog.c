639b62ee684af38d2d003b6df71bab8d6eff38b0
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-06-19 23:29:01 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-	printf("Age: %f\n", (d->age) ? d->age : "(nil)");
+	if (d->age != NULL)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: %s\n", "(nil)");
 	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
