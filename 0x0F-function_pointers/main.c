703640b7df9f7432c353b550f1fb6c90c310cb1b
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-21 14:23:40 -0700

    task 3

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,55 +1,43 @@
 #include <stdio.h>
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <string.h>
+#include "operations.h"
 
-/**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_98(int elem)
-{
-    return (98 == elem);
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Error\n");
+        return 98;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    char operator = argv[2][0];
+
+    int result;
+    if (operator == '+') {
+        result = add(num1, num2);
+    } else if (operator == '-') {
+        result = subtract(num1, num2);
+    } else if (operator == '*') {
+        result = multiply(num1, num2);
+    } else if (operator == '/') {
+        if (num2 == 0) {
+            printf("Error\n");
+            return 100;
+        }
+        result = divide(num1, num2);
+    } else if (operator == '%') {
+        if (num2 == 0) {
+            printf("Error\n");
+            return 100;
+        }
+        result = modulo(num1, num2);
+    } else {
+        printf("Error\n");
+        return 99;
+    }
+
+    printf("%d\n", result);
+    return 0;
 }
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-    return (elem > 0);
-}
-
-
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-    return (elem == 98 || -elem == 98);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-    int index;
-
-    index = int_index(array, 20, is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, abs_is_98);
-    printf("%d\n", index);
-    index = int_index(array, 20, is_strictly_positive);
-    printf("%d\n", index);
-    return (0);
-}
