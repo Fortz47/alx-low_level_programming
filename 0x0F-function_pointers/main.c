3fcecaad3693ac03cd01ececf8fdab996d6fe691
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-21 14:42:09 -0700

    task 3

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "operations.h"
+#include "3-calc.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Error\n");
-        return 98;
-    }
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 on success, or the corresponding exit status on failure
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[3]);
-    char operator = argv[2][0];
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    int result;
-    if (operator == '+') {
-        result = add(num1, num2);
-    } else if (operator == '-') {
-        result = subtract(num1, num2);
-    } else if (operator == '*') {
-        result = multiply(num1, num2);
-    } else if (operator == '/') {
-        if (num2 == 0) {
-            printf("Error\n");
-            return 100;
-        }
-        result = divide(num1, num2);
-    } else if (operator == '%') {
-        if (num2 == 0) {
-            printf("Error\n");
-            return 100;
-        }
-        result = modulo(num1, num2);
-    } else {
-        printf("Error\n");
-        return 99;
-    }
+	num1 = atoi(argv[1]);
+	 num2 = atoi(argv[3]);
 
-    printf("%d\n", result);
-    return 0;
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
 }
 
