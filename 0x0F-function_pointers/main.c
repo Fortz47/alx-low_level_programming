feae825dd432489fd0fee6eaf192a84a4431a673
Atuokwu <atuokwu.fortune@gmail.com>
2023-06-22 02:12:06 -0700

    task 4

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,44 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
+#include "function_pointers.h"
 
 /**
- * main - Entry point of the program
- * @argc: The number of arguments passed to the program
- * @argv: An array of pointers to the arguments
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
  *
- * Return: 0 on success, or the corresponding exit status on failure
+ * Return: 0 if false, something else otherwise.
  */
-int main(int argc, char *argv[])
+int is_98(int elem)
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	num1 = atoi(argv[1]);
-	 num2 = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		return (99);
-	}
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		return (100);
-	}
-
-	result = op_func(num1, num2);
-	printf("%d\n", result);
-
-	return (0);
+    return (98 == elem);
 }
 
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+    return (elem > 0);
+}
+
+
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int abs_is_98(int elem)
+{
+    return (elem == 98 || -elem == 98);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+    int index;
+
+    index = int_index(array, 20, is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, abs_is_98);
+    printf("%d\n", index);
+    index = int_index(array, 20, is_strictly_positive);
+    printf("%d\n", index);
+    return (0);
+}
