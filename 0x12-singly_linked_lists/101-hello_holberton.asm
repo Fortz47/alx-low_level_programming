a5a0807ab4f6b4c5595f1634cdd861b3c593768e
Atuokwu <atuokwu.fortune@gmail.com>
2023-07-03 16:14:59 -0700

    solved advanced tasks

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+section .data
+    hello_msg db "Hello, Holberton", 0
+    newline db 10
+
+section .text
+    global main
+    extern printf
+
+main:
+    ; Prepare the arguments for printf
+    mov rdi, hello_msg   ; Format string
+    xor rax, rax        ; Clear RAX (no floating-point arguments)
+    call printf         ; Call the printf function
+
+    ; Print a new line
+    mov rdi, newline    ; Format string for newline
+    xor rax, rax        ; Clear RAX (no floating-point arguments)
+    call printf         ; Call the printf function
+
+    ; Exit the program
+    xor rdi, rdi        ; Set exit code to 0
+    mov rax, 60         ; syscall number for exit
+    syscall             ; Invoke syscall to exit
+
