6b710ea5f6327533382032f7631de78cd3bc010f
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-08 17:51:45 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,6 +15,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	bit = 1UL << index;
-	n = n | bit;
+	*n = *n | bit;
 	return (1);
 }
