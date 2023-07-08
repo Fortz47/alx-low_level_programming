33a66ca7d6bde28492836a0d64946c8071df883d
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-08 18:02:45 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,21 +5,20 @@
  * @n: number
  * @index: index
  *
- * Return: the value of the  bit at a index or -1 if error
+ * Return: the value of the  bit at the index or -1 if error
  * occured.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i = (int) index;
+	unsigned long int mask;
+	unsigned long int bit;
 
-	while (n && i >= 0)
-	{
-		if (i == 0)
-			return (n & 1);
-		n = n >> 1;
-		i--;
-	}
-	return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	bit = n & mask;
+
+	return ((bit != 0) ? 1 : 0);
 }
 
 /**
@@ -33,24 +32,16 @@ int get_bit(unsigned long int n, unsigned int index)
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int bit_to_set;
+	unsigned long int bit;
 
-	int i = (int) index;
-	unsigned int bit = 1;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
 	bit_to_set = get_bit(*n, index);
-	if (bit_to_set == -1)
-		return (-1);
 	if (bit_to_set == 0)
 		return (1);
-	while (i >= 0)
-	{
-		if (i == 0)
-		{
-			*n = *n ^ bit;
-			return (1);
-		}
-		bit = bit << 1;
-		i--;
-	}
-	return (-1);
+
+	bit = 1UL << index;
+	*n = *n ^ bit;
+	return (1);
 }
