37199384f2716ebd033f439e40d87965b555df74
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-11 16:46:37 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,8 +31,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
-	if (fd == -1)
+	if (open(filename, O_RDONLY) == -1);
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+	}
+	else
 	{
 		fd = open(filename, O_WRONLY | O_TRUNC);
 		if (fd == -1)
