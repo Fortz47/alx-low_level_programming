1a9a7cf82b09fa3ede83317168a631584aa8dfa1
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-11 17:01:08 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,8 +32,21 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	if (fd_to == -1)
+	if (open(argv[2], O_RDONLY) == -1)
+	{
+		fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
+		if (fd_to == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			if (close(fd_from) == -1)
+			{
+				dprintf(2, "Error: Can't close fd %d\n", fd_from);
+				exit(100);
+			}
+			exit(99);
+		}
+	}
+	else
 	{
 		fd_to = open(argv[2], O_WRONLY | O_TRUNC);
 		if (fd_to == -1)
