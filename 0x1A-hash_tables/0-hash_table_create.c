d286d4c5f96f61b35f854d322593eaf46910ecad
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2023-09-08 14:23:34 +0000

    added all files and solved task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to hash table or Null if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t) + sizeof(hash_node_t) * size);
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	return (ht);
+}
